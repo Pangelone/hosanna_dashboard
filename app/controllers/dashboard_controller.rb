efc59778291dc8bde4bba714e4954e811cff144d
pangelone <pablo.angelone@hotmail.com>
2021-04-14 15:11:23 -0300

    add chart logic

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -23,11 +23,13 @@ class DashboardController < ApplicationController
 
 		response = http.request(request)
 
-		@users = JSON.parse(response.read_body)["users"]
-    @intro_ok = JSON.parse(response.read_body)["intro_ok"]
+    data = JSON.parse(response.read_body)
+
+		@users = data["users"]
+    @intro_ok = data["intro_ok"]
     @Intro_ok_porcentaje = (@intro_ok.to_f / @users.to_f * 100).round(2)
-    @monthly_active_purchases = JSON.parse(response.read_body)["monthly_active_purchases"]
-    @yearly_active_purchases = JSON.parse(response.read_body)["yearly_active_purchases"]
+    @monthly_active_purchases = data["monthly_active_purchases"]
+    @yearly_active_purchases = data["yearly_active_purchases"]
 
   end
 
@@ -50,80 +52,97 @@ class DashboardController < ApplicationController
   def pushes
     require "net/http"
 
-    if params[:lang].nil?
-      if params[:type].nil?
-        @lang = "all"
-        @type = "all"
-        @moment = "all"
+    @lang = params[:lang]
+    @type = params[:type]
+    @moment = params[:moment]
+    @interval = params[:interval]
 
-        if params[:interval].nil?
-          @interval = 1
-        else
-          @interval = params[:interval]
-        end
-
-         @pushes = [
-          contentor_pushes(144,"on_demand",@interval),
-          contentor_pushes(149,"on_demand",@interval),
-          contentor_pushes(150,"on_demand",@interval),
-          contentor_pushes(151,"on_demand",@interval),
-          contentor_pushes(152,"on_demand",@interval),
-          contentor_pushes(153,"on_demand",@interval),
-          contentor_pushes(144,"subscription",@interval),
-          contentor_pushes(149,"subscription",@interval),
-          contentor_pushes(150,"subscription",@interval),
-          contentor_pushes(151,"subscription",@interval),
-          contentor_pushes(152,"subscription",@interval),
-          contentor_pushes(153,"subscription",@interval)
-        ]
-        #render json: JSON.pretty_generate(@pushes)
-      else
-        @type = params[:type]
-        @lang = "all"
-        @moment = "all"
-        if params[:interval].nil?
-          @period = 1
-        else
-          @period = params[:interval]
-        end
-
-        @pushes = [
-          contentor_pushes(144,@type,@interval),
-          contentor_pushes(149,@type,@interval),
-          contentor_pushes(150,@type,@interval),
-          contentor_pushes(151,@type,@interval),
-          contentor_pushes(152,@type,@interval),
-          contentor_pushes(153,@type,@interval)
-        ]
-
-        #render json: JSON.pretty_generate(@pushes)
-      end
-    else
-      @lang = params[:lang]
-      @type = params[:type]
-      @moment = params[:moment]
-      if params[:interval].nil?
-        @interval = 1
-      else
-        @interval = params[:interval]
-      end
-      @app_id = CONTENTOR_HOSANNA_APP_ID[@lang.to_sym][@moment.to_sym]
-
-      @pushes = [contentor_pushes(@app_id,@type,@interval)] 
-      #render json: JSON.pretty_generate(@pushes)
+    if params[:type] = "all" && params[:lang] = "all" && params[:moment] = "all"
+      @pushes = [
+        contentor_pushes(144,"on_demand",@interval),
+        contentor_pushes(149,"on_demand",@interval),
+        contentor_pushes(150,"on_demand",@interval),
+        contentor_pushes(151,"on_demand",@interval),
+        contentor_pushes(152,"on_demand",@interval),
+        contentor_pushes(153,"on_demand",@interval),
+        contentor_pushes(144,"subscription",@interval),
+        contentor_pushes(149,"subscription",@interval),
+        contentor_pushes(150,"subscription",@interval),
+        contentor_pushes(151,"subscription",@interval),
+        contentor_pushes(152,"subscription",@interval),
+        contentor_pushes(153,"subscription",@interval)
+      ]
+    elsif params[:type] = "all" && params[:lang] = "all" && params[:moment] != "all"
+      @pushes = [
+        contentor_pushes(CONTENTOR_HOSANNA_APP_ID["es".to_sym][@moment.to_sym],"subscription",@interval),
+        contentor_pushes(CONTENTOR_HOSANNA_APP_ID["en".to_sym][@moment.to_sym],"subscription",@interval),
+        contentor_pushes(CONTENTOR_HOSANNA_APP_ID["pt".to_sym][@moment.to_sym],"subscription",@interval),
+        contentor_pushes(CONTENTOR_HOSANNA_APP_ID["es".to_sym][@moment.to_sym],"on_demand",@interval),
+        contentor_pushes(CONTENTOR_HOSANNA_APP_ID["en".to_sym][@moment.to_sym],"on_demand",@interval),
+        contentor_pushes(CONTENTOR_HOSANNA_APP_ID["pt".to_sym][@moment.to_sym],"on_demand",@interval),
+      ]    
+    elsif params[:type] = "all" && params[:lang] != "all" && params[:moment] = "all"
+      @pushes = [
+        contentor_pushes(CONTENTOR_HOSANNA_APP_ID[@lang.to_sym]["day".to_sym],"subscription",@interval),
+        contentor_pushes(CONTENTOR_HOSANNA_APP_ID[@lang.to_sym]["night".to_sym],"subscription",@interval),
+        contentor_pushes(CONTENTOR_HOSANNA_APP_ID[@lang.to_sym]["day".to_sym],"on_demand",@interval),
+        contentor_pushes(CONTENTOR_HOSANNA_APP_ID[@lang.to_sym]["night".to_sym],"on_demand",@interval),
+      ]   
+    elsif params[:type] = "all" && params[:lang] != "all" && params[:moment] != "all"
+      @pushes = [
+        contentor_pushes(CONTENTOR_HOSANNA_APP_ID[@lang.to_sym][@moment.to_sym],"subscription",@interval),
+        contentor_pushes(CONTENTOR_HOSANNA_APP_ID[@lang.to_sym][@moment.to_sym],"on_demand",@interval),
+      ]   
+    elsif params[:type] != "all" && params[:lang] = "all" && params[:moment] = "all"
+      @pushes = [
+        contentor_pushes(144,@type,@interval),
+        contentor_pushes(149,@type,@interval),
+        contentor_pushes(150,@type,@interval),
+        contentor_pushes(151,@type,@interval),
+        contentor_pushes(152,@type,@interval),
+        contentor_pushes(153,@type,@interval),
+        contentor_pushes(144,@type,@interval),
+        contentor_pushes(149,@type,@interval),
+        contentor_pushes(150,@type,@interval),
+        contentor_pushes(151,@type,@interval),
+        contentor_pushes(152,@type,@interval),
+        contentor_pushes(153,@type,@interval)
+      ]
+    elsif params[:type] != "all" && params[:lang] = "all" && params[:moment] != "all"
+      @pushes = [
+        contentor_pushes(CONTENTOR_HOSANNA_APP_ID["es".to_sym][@moment.to_sym],@type,@interval),
+        contentor_pushes(CONTENTOR_HOSANNA_APP_ID["en".to_sym][@moment.to_sym],@type,@interval),
+        contentor_pushes(CONTENTOR_HOSANNA_APP_ID["pt".to_sym][@moment.to_sym],@type,@interval),
+      ]    
+    elsif params[:type] != "all" && params[:lang] != "all" && params[:moment] = "all"
+      @pushes = [
+        contentor_pushes(CONTENTOR_HOSANNA_APP_ID[@lang.to_sym]["day".to_sym],@type,@interval),
+        contentor_pushes(CONTENTOR_HOSANNA_APP_ID[@lang.to_sym]["night".to_sym],@type,@interval),
+      ]  
+    elsif params[:type] != "all" && params[:lang] != "all" && params[:moment] != "all"
+      @pushes = [
+        contentor_pushes(CONTENTOR_HOSANNA_APP_ID[@lang.to_sym][@moment.to_sym],@type,@interval),
+      ]  
     end
-    @chart = []
+
+    @affected = []
+    @delivered  = []
+    @clicked = []
     @pushes.each do |pushess|
       pushess.each do |push|
-        @chart << [
+        @affected << [
           push["created_at"].to_date.to_s,
           push["affected_users"].to_f,
+        ]
+        @delivered << [
+          push["created_at"].to_date.to_s,
           push["delivered"].to_f,
+        ]
+        @clicked << [
+          push["created_at"].to_date.to_s,
           push["clicks"].to_f,
         ]
       end
     end
-
-    p @chart
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root 'dashboard#index'
-  get '/pushes/:type/:lang/:moment/(:interval)', to: 'dashboard#pushes'
-  get '/pushes/:type/all/(:interval)', to: 'dashboard#pushes'
-  get '/pushes/(:interval)', to: 'dashboard#pushes'
+  get '/pushes/(:type)/(:lang)/(:moment)/(:interval)', to: 'dashboard#pushes'
   get '/:lang/:period', to: 'dashboard#index'
 end
