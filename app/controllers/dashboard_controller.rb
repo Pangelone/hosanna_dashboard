e0a5b321c84e3ea5ee0f58dfdc5a437ed95c7dac
Pablo Angelone <pabloangelone@PablotekiMacBook-Pro.local>
2021-06-16 16:40:51 -0500

    tool remove premioum

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -146,4 +146,33 @@ class DashboardController < ApplicationController
       end
     end
   end
+
+    def tools
+        url_prod = "https://apihosanna.contentor.io"
+        url_test = "https://06e7b4d07a67.ngrok.io"
+
+        slug = params[:slug]
+
+        begin
+          if slug.nil?  || slug == ""
+            @msj = "Debe completar el slug"
+          else            
+            url = URI(url_test + "/v1/remove_premium/" + slug)
+            http = Net::HTTP.new(url.host, url.port)
+            http.use_ssl = true
+            http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+            
+            request = Net::HTTP::Post.new(url)
+            request["content-type"] = 'application/json'
+
+            response = http.request(request)
+
+            data = JSON.parse(response.read_body)
+            @msj = data["msj"]
+          end
+        rescue 
+          @msj = "Error de formato"
+        end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'dashboard#index'
   get '/pushes/(:type)/(:lang)/(:moment)/(:interval)', to: 'dashboard#pushes'
   get '/:lang/:period', to: 'dashboard#index'
+  get '/tools', to: 'dashboard#tools'
 end
